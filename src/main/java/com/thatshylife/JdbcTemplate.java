7b364cbc066d0b7b8782201d4c38de1632c7ff5a
Steve-Random <nanafokousteve@gmail.com>
2026-06-18 17:31:49 -0400

    Deployment Successful, Backend live!, correcting flutter base url

--- src/main/java/com/thatshylife/JdbcTemplate.java
+++
@@ -1,4 +0,0 @@
-package com.thatshylife;
-
-public class JdbcTemplate {
-}
--- that_shy_life_ui/lib/JournalService.dart
+++ that_shy_life_ui/lib/JournalService.dart
@@ -10,7 +10,7 @@ class JournalService {
     if(kIsWeb){
       return 'http://localhost:8080/api/entries';
     }else{
-      return 'http://10.0.2.2:8080/api/entries';
+      return 'https://that-shy-life-journaling-app.onrender.com/api/entries';
     }
   }
 
