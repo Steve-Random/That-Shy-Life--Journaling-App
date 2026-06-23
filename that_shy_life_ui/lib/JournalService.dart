e5f786ea18f17d430da1260c2fe219028e1756fd
Steve-Random <nanafokousteve@gmail.com>
2026-06-23 01:40:35 -0400

    Edited JournalService.dart to use the new Authentication feature Created LoginScreen.dart Created RegisterScreen.dart Edited ThatShyLifeApp.dart

--- that_shy_life_ui/lib/JournalService.dart
+++ that_shy_life_ui/lib/JournalService.dart
@@ -1,5 +1,6 @@
 import 'dart:convert';
 import 'package:http/http.dart' as http;
+import 'package:shared_preferences/shared_preferences.dart';
 import 'JournalEntry.dart';
 import 'package:flutter/foundation.dart';
 
@@ -7,13 +8,67 @@ class JournalService {
   // running locally for now (GET...http.get())
 
   static String get baseUrl{
-      return 'https://that-shy-life-journaling-app.onrender.com/api/entries';
+      return 'https://that-shy-life-journaling-app.onrender.com';
   }
+
+  //Token Storage
+  static Future<void> saveToken(String token) async {
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.setString('jwt_token', token);
+  }
+
+  static Future<String?> getToken() async {
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.getString('jwt_token');
+  }
+
+  static Future<void> deleteToken() async {
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.remove('jwt_token');
+  }
+
+  //Authentication
+  static Future<bool> register(String email, String password) async {
+    final response = await http.post(
+      Uri.parse('$baseUrl/api/auth/register'),
+      headers: {'Content-Type': 'application/json'},
+      body: jsonEncode({'email': email, 'password': password}),
+    );
+    if(response.statusCode == 200){
+      final data = jsonDecode(response.body);
+      await saveToken(data['token']);
+      return true;
+    }
+    return false;
+  }
+
+  static Future<bool> login(String email, String password) async {
+    final response = await http.post(
+      Uri.parse('$baseUrl/api/auth/register'),
+      headers: {'Content-Type': 'application/json'},
+      body: jsonEncode({'email': email, 'password': password}),
+    );
+    if(response.statusCode == 200){
+      final data = jsonDecode(response.body);
+      await saveToken(data['token']);
+      return true;
+    }
+    return false;
+  }
+
+
+  //Entries
 
   //Fetching all entries from Java
 Future<List<JournalEntry>> fetchEntries() async{
-  final response = await http.get(Uri.parse(baseUrl));
-
+    final token = await getToken();
+  final response = await http.get(
+      Uri.parse('$baseUrl/api/entries'),
+    headers: {
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer $token',
+    }
+  );
   if(response.statusCode == 200){
     List<dynamic> body = json.decode(response.body);
     return body.map((item) => JournalEntry.fromJson(item)).toList();
@@ -24,9 +79,13 @@ Future<List<JournalEntry>> fetchEntries() async{
 
 //Sending a new reflection to Java (POST...http.post())
 Future<void> saveEntry(JournalEntry entry) async{
+    final token = await getToken();
   await http.post(
-    Uri.parse(baseUrl),
-    headers: {'Content-Type': 'application/json'},
+    Uri.parse('$baseUrl/api/entries'),
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': 'Bearer $token',
+    },
     body: jsonEncode(entry.toJson()),
   );
 }
--- that_shy_life_ui/lib/ThatShyLifeApp.dart
+++ that_shy_life_ui/lib/ThatShyLifeApp.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'JournalFeedScreen.dart';
+import 'JournalService.dart';
+import 'LoginScreen.dart';
 import 'app_theme.dart';
 
 class ThatShyLifeApp extends StatelessWidget {
@@ -11,7 +13,20 @@ class ThatShyLifeApp extends StatelessWidget {
       title: 'That Shy Life',
       debugShowCheckedModeBanner: false,
       theme: AppTheme.theme,
-      home: const JournalFeedScreen(),
+      home: FutureBuilder<String?>(
+          future: JournalService.getToken(),
+          builder: (context, snapshot){
+            if(snapshot.connectionState == ConnectionState.waiting){
+              return const Scaffold(
+                body: Center(child: CircularProgressIndicator()),
+              );
+            }
+            if(snapshot.data != null){
+              return const JournalFeedScreen();
+            }
+            return const LoginScreen();
+          },
+      ),
     );
   }
 }
