45012a54df86e2ef8fef7c784b2a59df63383b97
Steve-Random <nanafokousteve@gmail.com>
2026-06-24 20:56:23 -0400

    Edited JournalFeedScreen.dart

--- that_shy_life_ui/build/web/flutter_bootstrap.js
+++ that_shy_life_ui/build/web/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"4c525dac5ebe5971c5708ef73558ed8edcf4a3
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3924950853" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1210330961" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
