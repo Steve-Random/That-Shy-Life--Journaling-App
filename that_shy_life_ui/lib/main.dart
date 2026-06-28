ce81661cef2638a3265a4076ed8370fe666be1cf
Steve-Random <nanafokousteve@gmail.com>
2026-06-28 00:14:16 -0400

    Fixing blank screen caused by unhandled Social battery null or empty

--- that_shy_life_ui/lib/main.dart
+++ that_shy_life_ui/lib/main.dart
@@ -5,9 +5,9 @@ import 'firebase_options.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
+  /*await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
-  );
+  );*/
   runApp(const ThatShyLifeApp());
 }
 
