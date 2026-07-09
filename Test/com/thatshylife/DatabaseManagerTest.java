9b14ce09fdd4fa2abe913d7e6458b182c61f3cee
Steve-Random <nanafokousteve@gmail.com>
2026-07-09 00:15:40 -0400

    Startung DatabaseManagerTest

---
+++ Test/com/thatshylife/DatabaseManagerTest.java
@@ -0,0 +1,37 @@
+package com.thatshylife;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DatabaseManagerTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void createUsersTable() {
+    }
+
+    @Test
+    void saveUser() {
+    }
+
+    @Test
+    void findUserByEmail() {
+    }
+
+    @Test
+    void createNewTable() {
+    }
+
+    @Test
+    void saveEntry() {
+    }
+
+    @Test
+    void getAllEntries() {
+    }
+}
